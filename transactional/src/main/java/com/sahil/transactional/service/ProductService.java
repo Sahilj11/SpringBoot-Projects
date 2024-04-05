9d7eba60e712dbd35abaa472abb96a897139c579
Sahil jalandhara <sahiljalandhara123@gmail.com>
2024-04-05 08:52:51 +0530

    Added some theory for transactional

--- transactional/src/main/java/com/sahil/transactional/service/ProductService.java
+++ transactional/src/main/java/com/sahil/transactional/service/ProductService.java
@@ -15,12 +15,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProductService {
 
-   private final ProductRepo productRepo; 
+    private final ProductRepo productRepo;
 
-   @Transactional
-   public void addProduct(Product product,String name){
-       product.setName(name);
-       productRepo.save(product);
-       throw new RuntimeException(":(");
-   }
+    @Transactional
+    // can use @Transactional(rollBackOn) to add Exception for which it should be
+    // rolled back
+    // by default it is for RuntimeException
+    // Exception should be thrown outside method to make use of @Transactional ,
+    // meaning using try-catch
+    // will not work for rollback
+    public void addProduct(Product product, String name) {
+        product.setName(name);
+        productRepo.save(product);
+        throw new RuntimeException(":(");
+        // try {
+        // throw new RuntimeException();
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+    }
 }
