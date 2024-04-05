8d8ddc43d54023e8a621369eaebdb22b6b4efc90
Sahil jalandhara <sahiljalandhara123@gmail.com>
2024-04-05 08:00:26 +0530

    Initial Commit

---
+++ transactional/src/main/java/com/sahil/transactional/TransactionalApplication.java
@@ -0,0 +1,13 @@
+package com.sahil.transactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionalApplication.class, args);
+	}
+
+}
