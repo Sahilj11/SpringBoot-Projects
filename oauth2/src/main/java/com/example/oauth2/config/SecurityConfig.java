bb60ffd547052cc39cabcdcfbae62ecbc4273a12
Sahil jalandhara <sahiljalandhara123@gmail.com>
2024-05-06 21:49:17 +0530

    added some mysql properties and changed security config

--- oauth2/src/main/java/com/example/oauth2/config/SecurityConfig.java
+++ oauth2/src/main/java/com/example/oauth2/config/SecurityConfig.java
@@ -3,47 +3,33 @@ package com.example.oauth2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
 /** SecurityConfig */
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    @Order(1)
-    public SecurityFilterChain asSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.exceptionHandling(
-                e -> e.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")));
-        return http.build();
-    }
+  @Bean
+  @Order(1)
+  public SecurityFilterChain asecurityFilterChain(HttpSecurity http) throws Exception {
+    http.exceptionHandling(
+        c ->
+            c.defaultAuthenticationEntryPointFor(
+                new LoginUrlAuthenticationEntryPoint("/login"),
+                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)));
+    return http.build();
+  }
 
-    @Bean
-    @Order(2)
-    public SecurityFilterChain appSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.formLogin();
-        http.authorizeHttpRequests(url -> url.anyRequest().authenticated());
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails u1 = User.withUsername("sahil")
-            .password(pEncoder().encode("1234"))
-            .authorities("read")
-            .build();
-        return new InMemoryUserDetailsManager(u1);
-    }
-
-    @Bean
-    public PasswordEncoder pEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  @Order
+  public SecurityFilterChain appSecurityFilterChain(HttpSecurity http) throws Exception {
+    http.formLogin(Customizer.withDefaults());
+    http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+    return http.build();
+  }
 }
